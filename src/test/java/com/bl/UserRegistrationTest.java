47afd4ca521d5a564dd8ac6ba8fb8f27058bc2db
Bhavesh Kadam <bkadam357@gmail.com>
2020-04-19 15:17:46 +0530

    ADDED: tests to validate Last name

--- src/test/java/com/bl/UserRegistrationTest.java
+++ src/test/java/com/bl/UserRegistrationTest.java
@@ -5,19 +5,30 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
     @Test
-    public void givenName_WhenName_ShouldReturnTrue() {
+    public void givenName_WhenFirstName_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
         Assert.assertTrue(userRegistration.validateName("Bhavesh"));
     }
 
-    public void givenName_WhenName_ShouldReturnFalse() {
+    public void givenName_WhenFirstName_ShouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
         Assert.assertFalse(userRegistration.validateName("bhaVesh"));
+    }
+    public void givenName_WhenLastName_ShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        Assert.assertTrue(userRegistration.validateName("Kadam"));
+    }
+
+    public void givenName_WhenLastName_ShouldReturnFalse() {
+        UserRegistration userRegistration = new UserRegistration();
+        Assert.assertFalse(userRegistration.validateName("kadam"));
     }
 
     public static void main(String[] args){
         UserRegistrationTest userRegistrationTest = new UserRegistrationTest();
-        userRegistrationTest.givenName_WhenName_ShouldReturnFalse();
-        userRegistrationTest.givenName_WhenName_ShouldReturnTrue();
+        userRegistrationTest.givenName_WhenFirstName_ShouldReturnFalse();
+        userRegistrationTest.givenName_WhenFirstName_ShouldReturnTrue();
+        userRegistrationTest.givenName_WhenLastName_ShouldReturnFalse();
+        userRegistrationTest.givenName_WhenLastName_ShouldReturnTrue();
     }
 }
